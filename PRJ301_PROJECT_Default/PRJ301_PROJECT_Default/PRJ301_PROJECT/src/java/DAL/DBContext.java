e8dcb22ebac6ee86d81710f214236a6eb30f0b13
alphabet11 <trantientn03@gmail.com>
2024-10-01 10:55:08 +0700

    new

--- PRJ301_PROJECT_Default/PRJ301_PROJECT_Default/PRJ301_PROJECT/src/java/DAL/DBContext.java
+++ PRJ301_PROJECT_Default/PRJ301_PROJECT_Default/PRJ301_PROJECT/src/java/DAL/DBContext.java
@@ -15,7 +15,7 @@ public class DBContext {
             //Change the username password and url to connect your own database
             String username = "sa";
             String password = "123";
-            String url = "jdbc:sqlserver://localhost:1433;databaseName=Asignment_PRJ301";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=project1";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, username, password);
         } catch (ClassNotFoundException | SQLException ex) {
